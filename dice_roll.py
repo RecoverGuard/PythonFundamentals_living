5b4d268a84dff54351a23a08a3c2b0442e22048f
james vinciguerra <james.vinciguerra@outlook.com>
2020-07-22 08:58:14 -0700

    dice roll

---
+++ dice_roll.py
@@ -0,0 +1,24 @@
+#this is a dice roll simulator
+
+import random
+
+min = 1
+max = 6
+
+roll_again = 'Y'
+good_bye = 'N'
+
+
+while roll_again == 'Y':
+    print('Rolling the dice...')
+    print('You rolled:')
+    print(random.randint(min, max))
+    print(random.randint(min, max))
+
+    roll_again = (input('Roll the dice again? Y/N '))
+
+while good_bye == 'N':
+    print(input('Good-Bye'))
+
+
+
