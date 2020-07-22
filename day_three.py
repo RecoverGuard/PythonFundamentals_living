8810e58bf48b07100cbf12a9049f79b9fe1603f7
james vinciguerra <james.vinciguerra@outlook.com>
2020-07-22 12:50:58 -0700

    day 3 labs

---
+++ day_three.py
@@ -0,0 +1,33 @@
+#basic labs for day three
+
+a = 13
+b = 25
+
+#mathematical operations
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a % b)
+
+#creating and indexing a string
+x = 'python'
+
+print(x[0])
+print(x[-1])
+print(len(x))
+print(x.upper())
+
+#creating and appending a list
+
+nums = []
+nums.append(10)
+nums.append(100)
+nums.append(10)
+nums.append(500)
+nums.append(56)
+print(nums)
+nums.pop()
+print(nums)
+nums.sort()
+print(nums)
