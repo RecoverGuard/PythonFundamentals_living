7fa40d20c23bcb7edb98652418dd24c6a1d4452a
james vinciguerra <james.vinciguerra@outlook.com>
2020-07-23 10:26:32 -0700

    basic if test

---
+++ testing_if_statements.py
@@ -0,0 +1,7 @@
+#basic test of using if statements
+
+hostname = input('What value should we set for hostname? ')
+
+if hostname.lower() == 'cheetah':
+    print('The hostname was found to be cheetah')
+    print('hostname matches expected config')
